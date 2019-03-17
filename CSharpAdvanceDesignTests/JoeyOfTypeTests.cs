ce72ef933a85bfcfa9b57627d63073990027c634
ching.miou <ching.miou@gmail.com>
2019-03-17 14:10:43 +0800

    OfType

--- CSharpAdvanceDesignTests/JoeyOfTypeTests.cs
+++ CSharpAdvanceDesignTests/JoeyOfTypeTests.cs
@@ -2,12 +2,13 @@
 using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
     public class JoeyOfTypeTests
     {
         [Test]
@@ -17,13 +18,64 @@ namespace CSharpAdvanceDesignTests
 
             var arguments = new Dictionary<string, object>
             {
+                {"validator1", new ProfitValidator()},
+                {"validator2", new ProductPriceValidator()},
                 {"model", new Product {Price = 100, Cost = 111}},
-                {"validator", new ProductValidator()},
             };
 
-            //var validators = JoeyOfType(?);
+            var validators = JoeyOfType<IValidator<Product>>(arguments.Values);
 
-            //Assert.AreEqual(1, validators.Count());
+            Assert.AreEqual(2, validators.Count());
+        }
+
+        [Test]
+        public void validate_special_type_value_from_arguments()
+        {
+            //ActionExecutingContext.ActionArguments: Dictionary<string,object>
+
+            var arguments = new Dictionary<string, object>
+            {
+                {"validator1", new ProfitValidator()},
+                {"validator2", new ProductPriceValidator()},
+                {"model", new Product {Price = 100, Cost = 111}},
+            };
+
+            var validators = JoeyOfType<IValidator<Product>>(arguments.Values);
+
+            var product = JoeyOfType<Product>(arguments.Values).Single();
+
+            var isValid = validators.All(x => x.Validate(product));
+
+            Assert.IsFalse(isValid);
+        }
+
+        private IEnumerable<TResult> JoeyOfType<TResult>(IEnumerable values)
+        {
+            var enumerator = values.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (current is TResult cast)
+                {
+                    yield return cast;
+                }
+            }
+        }
+    }
+
+    public class ProductPriceValidator : IValidator<Product>
+    {
+        public bool Validate(Product model)
+        {
+            return model.Price - model.Cost >= 0;
+        }
+    }
+
+    public class ProfitValidator : IValidator<Product>
+    {
+        public bool Validate(Product model)
+        {
+            return model.Price > 0;
         }
     }
 }
