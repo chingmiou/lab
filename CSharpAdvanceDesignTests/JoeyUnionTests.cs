66d05415cb29722fd8e5253e599cd0e6b50d83d0
ching.miou <ching.miou@gmail.com>
2019-03-17 10:22:00 +0800

    Union

--- CSharpAdvanceDesignTests/JoeyUnionTests.cs
+++ CSharpAdvanceDesignTests/JoeyUnionTests.cs
@@ -5,14 +5,13 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyUnionTests
     {
         [Test]
         public void union_numbers()
         {
-            var first = new[] { 1, 3, 5 };
-            var second = new[] { 5, 3, 7 };
+            var first = new[] { 1, 3, 5, 3, 1 };
+            var second = new[] { 5, 3, 7, 7 };
 
             var actual = JoeyUnion(first, second);
             var expected = new[] { 1, 3, 5, 7 };
@@ -22,7 +21,26 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<int> JoeyUnion(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var hashSet = new HashSet<int>();
+            var firstEnumerator = first.GetEnumerator();
+            while (firstEnumerator.MoveNext())
+            {
+                var firstCurrent = firstEnumerator.Current;
+                if (hashSet.Add(firstCurrent))
+                {
+                    yield return firstCurrent;
+                }
+            }
+
+            var secondEnumerator = second.GetEnumerator();
+            while (secondEnumerator.MoveNext())
+            {
+                var secondCurrent = secondEnumerator.Current;
+                if (hashSet.Add(secondCurrent))
+                {
+                    yield return secondCurrent;
+                }
+            }
         }
     }
 }
